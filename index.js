3d0ab9f5458409d0c3b914862b732b127b47dc91
Ira Levkovich <6346390@mail.ru>
2020-09-08 20:26:35 +0300

    Fix

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 let addMessage = document.querySelector(".message");
 let addMessageDescription = document.querySelector(".message_description");
-let addButton = document.querySelector(".add");
+const addButton = document.querySelector(".add");
 let todo = document.querySelector(".todo");
-let editButton = document.querySelector(".editButton")
+const editButton = document.querySelector(".editButton")
 
 let todoList = [];
 
 addButton.addEventListener("click", function () {
 
   let newTodo = {
-    todo: addMessage.value + ": " + addMessageDescription.value,
-    checked: false,
+    todo: addMessage.value + ": " + addMessageDescription.value
   };
 
   todoList.push(newTodo);
@@ -19,29 +18,15 @@ addButton.addEventListener("click", function () {
 
 function displayMessages() {
   let displayMessage = "";
-  todoList.forEach(function (item, i) {
+  todoList.forEach(function (item) {
     displayMessage += `
     <li>
-    <input type="checkbox" id="item_${ i}" ${item.checked ? "checked" : ""}>
-    <label label for = "item_${ i}">${item.todo}</label>
-    </li>
+    ${item.todo}
+    <button class="button_image editButton"></button>
+    <button class="button_image progressButton"></button>
+    <button class="button_image deleteButton"></button>
+    </li> 
     `;
     todo.innerHTML = displayMessage;
   });
 }
-
-todo.addEventListener("change", function (event) {
-  let idInput = event.target.getAttribute("id");
-});
-
-todo.addEventListener("change", function (event) {
-  let idInput = event.target.getAttribute("id");
-  let forLabel = todo.querySelector('[for=' + idInput + ']');
-  let valueLabel = forLabel.innerHTML;
-
-  todoList.forEach(function (item) {
-    if (item.todo === valueLabel) {
-      item.checked = !item.checked;
-    }
-  });
-});
